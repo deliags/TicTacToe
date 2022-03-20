e9ea27c23d2595d88de02a7d07a6d73ccf7350d0
deliags <deliagsmith@outlook.com>
2022-03-20 17:12:13 +0000

    working solution

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const gameLoop = (() => {
+  let playerTwoTurn;
   const xClass = 'x';
   const oClass = 'o';
-  let playerOneTurn;
+  const cells = document.querySelectorAll('[data-cell]');
+  const resultMessage = document.getElementById('game-result');
+  const resultText = document.querySelector('[data-game-result-text]');
+  const restartBtn = document.getElementById('restartBtn');
 
   const GameBoard = () => {
     const board = document.getElementById('board');
-    const cells = document.querySelector('[data-cell]');
     const winningCombo = [
       [0, 1, 2],
       [3, 4, 5],
@@ -15,59 +18,95 @@ const gameLoop = (() => {
       [3, 6, 9],
       [0, 4, 8],
       [2, 4, 6]
-    ]
+    ];
     return {
       board,
       cells,
       winningCombo
+    };
+  };
+
+  const setHoverClass = () => {
+    board.classList.remove(xClass);
+    board.classList.remove(oClass);
+    if (playerTwoTurn) {
+      board.classList.add(oClass);
+    } else {
+      board.classList.add(xClass);
     }
   };
 
-  const handleClick = ((e) => {
+  function handleClick(e) {
     const cell = e.target;
-    const currentClass = playerOneTurn ? xClass : oClass;
+    const currentClass = playerTwoTurn ? oClass : xClass;
     placeMark(cell, currentClass);
-  })()
+    //to take out
+    if (checkWin(currentClass)) {
+      showResult(false);
+    } else if (isDraw()) {
+      showResult(true);
+    } else {
+      swapTurns();
+      setHoverClass();
+    }
+    return {
+      cell,
+      currentClass
+    }
+  };
 
-  const placeMark = ((cell, currentClass) => {
-    cell.classList.add(currentClass)
-  })()
+  const placeMark = (cell, currentClass) => {
+    cell.classList.add(currentClass);
+  };
 
-  const resetBoard = (() => {
+  const resetBoard = () => {
     cells.forEach(cell => {
-      cell.classList.remove(xClass)
-      cell.classList.remove(oClass)
-      cell.removeEventListener('click', handleClick)
+      cell.classList.remove(xClass);
+      cell.classList.remove(oClass);
+      cell.removeEventListener('click', handleClick);
       cell.addEventListener('click', handleClick, {
         once: true
       });
-    })
-  })()
+    });
+  };
 
-  const setHoverClass = (() => {
-    board.classList.remove(xClass)
-    board.classList.remove(oClass)
-    if (playerOneTurn) {
-      board.classList.add(xClass);
-    } else {
-      board.classList.add(oClass);
-    }
-  })();
-
-  const startGame = (() => {
-    playerOneTurn = false;
+  const startGame = () => {
+    playerTwoTurn = false;
     resetBoard();
     setHoverClass();
-  })()
+    resultMessage.classList.remove('show')
+  };
 
-  const showResult = () => {
-    const resultMessage = document.getElementById('game-result');
-    const resultText = document.querySelector('[data-game-result-text]')
-    const restartBtn = document.getElementById('restartBtn');
+  const showResult = draw => {
     restartBtn.addEventListener('click', startGame);
+
+    if (draw) {
+      resultText.textContent = 'Draw!';
+    } else {
+      resultText.textContent = `${playerTwoTurn ? "Player Two" : "Player One"} Wins!`;
+    }
+    resultMessage.classList.add('show');
+  };
+
+
+
+  const isDraw = () => {
+    return [...cells].every(cell => {
+      return cell.classList.contains(xClass) || cell.classList.contains(oClass);
+    })
   }
 
+  const swapTurns = () => {
+    playerTwoTurn = !playerTwoTurn;
+  }
 
+  const checkWin = (currentClass) => {
+    return GameBoard().winningCombo.some(combo => {
+      return combo.every(index => {
+        return cells[index].classList.contains(currentClass);
+      })
+    })
+  }
 
-
+  startGame();
 })();
